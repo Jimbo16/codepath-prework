f770cfeeab97f9f266fac3d1b046ba3f6d772ed2
Jimmy Ocaya <70233548+Jimbo16@users.noreply.github.com>
2023-06-10 12:22:08 -0400

    Added prework file to reppository

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Jimmy Ocaya on 6/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func ChangeBackgroundColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+        func changeColor() ->UIColor{
+            let red = CGFloat.random(in: 0...1)
+            let green = CGFloat.random(in: 0...1)
+            let blue=CGFloat.random(in:0...1)
+            
+            return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+        }
+    }
+}
+
+
